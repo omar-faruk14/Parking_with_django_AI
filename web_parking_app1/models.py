4524c7e70713dd09745d585222478457d6c3cba0
Omar Faruk <Omarfaruk885@gmail.com>
2023-06-04 16:40:19 +0600

    Retrive Data from Database

--- web_parking_app1/models.py
+++ web_parking_app1/models.py
@@ -4,7 +4,6 @@ class ParkingLot(models.Model):
     name = models.CharField(max_length=255)
     number_of_parking = models.IntegerField()
     parking_limit = models.IntegerField()
-    parking_number_ratio = models.IntegerField()
 
     def __str__(self):
         return self.name
--- web_parking_app1/views.py
+++ web_parking_app1/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import ParkingLot
+
 
 def home(request):
-    return render(request,"carreview.html")
+    parking_lots=ParkingLot.objects.all()
+    return render(request,"carreview.html",{'parking_lots':parking_lots})
